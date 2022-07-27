f28244bd9fceea9c57bd563ca210d29c99a88ab2
Alan Costa Araujo <alancostaaraujo@hotmail.com>
2022-07-27 00:36:13 +0000

    Aula 3

--- app.py
+++ app.py
@@ -1,12 +1,27 @@
 from flask import Flask, render_template
+from datetime import datetime
 
 app = Flask("hello")
 
-@app.route("/")
-@app.route("/hello")
-def hello():
-    return "Hello World"
+posts = [
+    {
+        "title": "O meu primeiro post",
+        "body": "Aqui é o texto do post",
+        "author": "Alan Costa Araujo",
+        "created": datetime(2022,7,25)
+    },
+        {
+        "title": "O meu segundo post",
+        "body": "Aqui é o texto do post",
+        "author": "Xikim Araujo",
+        "created": datetime(2022,7,26)
+    },
+]
 
-@app.route("/meucontato")
-def meuContato():
-    return render_template ('index.html')
+@app.route("/")
+def index():
+    return render_template("index.html", posts=posts)
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
